ca5d5fd9d90f07c951ec5a261b0e17cea13ceb01
JoacoCinnamon <joacopontello2002@gmail.com>
2023-04-22 18:48:08 -0300

    Clerk en español y Prisma 4.13

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,30 @@
 import { type AppType } from "next/app";
+import type { Metadata } from "next";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { esES } from "@clerk/localizations";
+
+export const metadata: Metadata = {
+  title: {
+    default: "Etiquetando",
+    template: `%s - Creando etiquetas`,
+  },
+  description: "",
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkProvider {...pageProps}>
+    <ClerkProvider localization={esES} {...pageProps}>
       <Component {...pageProps} />
     </ClerkProvider>
   );
